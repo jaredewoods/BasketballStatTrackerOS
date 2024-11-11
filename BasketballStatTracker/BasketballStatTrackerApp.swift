6645bd85d318ca41c6af115aeb89d771c1f2da8f
jaredwoods <jaredwoods23@icloud.com>
2024-11-11 11:23:32 -0600

    Initial Commit

---
+++ BasketballStatTracker/BasketballStatTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballStatTrackerApp.swift
+//  BasketballStatTracker
+//
+//  Created by Jared Woods on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BasketballStatTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasketballStatTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasketballStatTracker
+//
+//  Created by Jared Woods on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
